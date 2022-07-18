933fdab30b692abfcabd0aa78b23b0eec15520bf
Shayan <sa@papershift.com>
2022-07-18 18:45:25 +0200

    Design initial game screens
    - Add home view & game views with respective view controllers

---
+++ WordGame/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  WordGame
+//
+//  Created by Shayan Ali on 18.07.22.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- WordGame/HomeViewController.swift
+++ WordGame/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  WordGame
 //
 //  Created by Shayan Ali on 18.07.22.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
 }
 
